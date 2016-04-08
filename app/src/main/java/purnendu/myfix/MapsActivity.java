e926dccdbe9fa630a470311d21ee73ecb9fc34c4
Purnendu Mukherjee <purnendu@rechargers.co.in>
2016-04-08 16:10:37 +0530

    Center fixed marker placed. But yet to make the map movement smooth.

--- app/src/main/java/purnendu/myfix/MapsActivity.java
+++ app/src/main/java/purnendu/myfix/MapsActivity.java
@@ -3,12 +3,16 @@ package purnendu.myfix;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements
+        GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
@@ -60,6 +64,25 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        LatLng latLng=new LatLng(18.521278,73.9254039);
+
+        CameraUpdate camera= CameraUpdateFactory.newLatLngZoom(latLng,12);
+        mMap.moveCamera(camera);
+
+        MarkerOptions options=new MarkerOptions()
+                .position(latLng)
+                .title("Annexe, Magarpatta")
+                .snippet("This is where Purnendu lives.")
+                .draggable(true);
+
+        mMap.addMarker(options);
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 }
